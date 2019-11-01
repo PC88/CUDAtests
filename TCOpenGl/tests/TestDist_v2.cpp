f36e0892be2325a9aab92ff2ea3943926ac33f86
PC88 <32823980+PC88@users.noreply.github.com>
2019-11-01 04:01:41 +0000

    dist 2 added

--- TCOpenGl/tests/TestDist_v2.cpp
+++ TCOpenGl/tests/TestDist_v2.cpp
@@ -6,7 +6,53 @@ TestDist_v2::TestDist_v2()
 {
 }
 
-
 TestDist_v2::~TestDist_v2()
 {
+
+}
+
+float TestDist_v2::scale(int i, int n)
+{
+	return ((float)i) / (n - 1);
+}
+
+float TestDist_v2::distance(float x1, float x2)
+{
+	return sqrt((x2 - x1) * (x2 - x1));
+}
+
+void TestDist_v2::distanceArray(float* out, float* in, float ref, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		out[i] = distance(in[i], ref);
+	}
+}
+
+void TestDist_v2::OnUpdate(float deltaTime)
+{
+	float* in = (float*)calloc(N, sizeof(float));
+	float* out = (float*)calloc(N, sizeof(float));
+	const float ref = 0.5f;
+
+	for (int i = 0; i < N; ++i)
+	{
+		in[i] = scale(i, N);
+	}
+
+	distanceArray(out, in, ref, N);
+
+	// release the heap memory after we are done using it
+	free(in);
+	free(out);
+}
+
+void TestDist_v2::OnRender()
+{
+
+}
+
+void TestDist_v2::OnImGuiRender()
+{
+
 }
--- TCOpenGl/tests/TestDist_v2.h
+++ TCOpenGl/tests/TestDist_v2.h
@@ -1,10 +1,27 @@
 #pragma once
 #include "Test.h"
+#include <math.h>
+#include <stdlib.h>
+#define N 20000000
+
 class TestDist_v2 :
 	public Test
 {
 public:
 	TestDist_v2();
 	virtual ~TestDist_v2();
+
+	// A scaling function to convert integers 0,1,......,N-1
+	// to evenly spaced floats ranging from 0 to 1
+	float scale(int i, int n);
+
+	// compute the distance between 2 points on a line
+	float distance(float x1, float x2);
+
+	void distanceArray(float* out, float* in, float ref, int n);
+
+	void OnUpdate(float deltaTime) override;
+	void OnRender() override;
+	void OnImGuiRender() override;
 };
 
